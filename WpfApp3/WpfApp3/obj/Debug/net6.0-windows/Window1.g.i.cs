6f47ccf847ffde855e5f1ccb370a7d25f120b9f5
Garrett <45549317+NixieSqwa@users.noreply.github.com>
2023-10-18 10:58:28 -0500

    Worked on login page xaml
    almost completed, needs the backend and selection. the password box needs to be formatted to rounded edges could not figure out how

--- WpfApp3/WpfApp3/obj/Debug/net6.0-windows/Window1.g.i.cs
+++ WpfApp3/WpfApp3/obj/Debug/net6.0-windows/Window1.g.i.cs
@@ -53,7 +53,7 @@ namespace WpfApp3 {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WpfApp3;V1.0.0.0;component/window1.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WpfApp3;component/window1.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\Window1.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
