3773b9cb3e0c6bfd5e703a90ae77d3bad5868483
Aidan <stilesaidan@gmail.com>
2023-10-30 10:19:27 -0500

    Added Features to Login Window
    Added Username: admin and PW: 1234 to login window, as well as error message upon incorrect input in either username or password. Also added a peek button to password input, click and hold to preview the password.

--- BlissEditor/MainWindow.xaml.cs
+++ BlissEditor/MainWindow.xaml.cs
@@ -24,6 +24,8 @@ namespace BlissEditor
         public MainWindow()
         {
             InitializeComponent();
+            lblIncorrect.Visibility = Visibility.Hidden;
+            txbPasswordShow.Visibility = Visibility.Hidden;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -33,10 +35,35 @@ namespace BlissEditor
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            /* Code Below from https://stackoverflow.com/questions/21706226/how-to-navigate-between-windows-in-wpf */
-            var newWindow = new RTEWindow(); //create your new form.
-            newWindow.Show(); //show the new form.
-            this.Close(); //only if you want to close the current form.
+            if ((txbUsername.Text == "admin") && (txbPassword.Password == "1234"))
+            {
+                /* Code Below from https://stackoverflow.com/questions/21706226/how-to-navigate-between-windows-in-wpf */
+                var newWindow = new RTEWindow(); //create your new form.
+                newWindow.Show(); //show the new form.
+                this.Close(); //only if you want to close the current form.
+            }
+            else
+            {
+                lblIncorrect.Visibility = Visibility.Visible;
+                txbPassword.Clear();
+            }
+        }
+
+        private void txbPassword_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            txbPasswordShow.Text = txbPassword.Password;
+        }
+
+        private void btnShow_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            txbPassword.Visibility = Visibility.Hidden;
+            txbPasswordShow.Visibility= Visibility.Visible;
+        }
+
+        private void btnShow_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            txbPassword.Visibility = Visibility.Visible;
+            txbPasswordShow.Visibility = Visibility.Hidden;
         }
     }
 }
